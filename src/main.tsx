47c587620e1947552397ac7787fce95546125cf6
Newish0 <80089521+Newish0@users.noreply.github.com>
2023-10-02 21:55:46 -0700

    fix: switch to use hash router
    This is because gh-pages is designed for static pages, and doesn't work well with url history.

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom/client";
 import Root from "@routes/Root.tsx";
 
 import "./index.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createHashRouter } from "react-router-dom";
 import ErrorPage from "@routes/ErrorPage";
 import App from "@routes/App";
 import Settings from "@routes/Settings";
 
 
-const router = createBrowserRouter([
+const router = createHashRouter([
     {
         path: "/",
         element: <Root />,
