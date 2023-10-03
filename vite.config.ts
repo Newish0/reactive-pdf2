bd05c3e4e1717ac811a363170d1a91e2ae6df209
Newish0 <80089521+Newish0@users.noreply.github.com>
2023-10-02 21:38:31 -0700

    build: set base for gh-page in prod build

--- vite.config.ts
+++ vite.config.ts
@@ -2,26 +2,30 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-const BASE_URL = process.env.APP_ENV === "local" ? "" : process.env.GH_PAGE_BASE;
+const GH_PAGE_BASE = "https://newish0.github.io/reactive-pdf2";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: [
-            { find: "@", replacement: path.resolve(__dirname, "src") },
-            { find: "@util", replacement: path.resolve(__dirname, "src/util") },
-            { find: "@type", replacement: path.resolve(__dirname, "src/type") },
-            { find: "@hooks", replacement: path.resolve(__dirname, "src/hooks") },
-            { find: "@components", replacement: path.resolve(__dirname, "src/components") },
-            { find: "@routes", replacement: path.resolve(__dirname, "src/routes") },
-            { find: "@atoms", replacement: path.resolve(__dirname, "src/atoms") },
-            { find: "@workarounds", replacement: path.resolve(__dirname, "src/workarounds") },
-            {
-                find: "tailwind-config",
-                replacement: path.resolve(__dirname, "./tailwind.config.js"),
-            },
-        ],
-    },
-    base: BASE_URL,
+export default defineConfig(({ command, mode }) => {
+    console.log(command, mode);
+
+    return {
+        plugins: [react()],
+        resolve: {
+            alias: [
+                { find: "@", replacement: path.resolve(__dirname, "src") },
+                { find: "@util", replacement: path.resolve(__dirname, "src/util") },
+                { find: "@type", replacement: path.resolve(__dirname, "src/type") },
+                { find: "@hooks", replacement: path.resolve(__dirname, "src/hooks") },
+                { find: "@components", replacement: path.resolve(__dirname, "src/components") },
+                { find: "@routes", replacement: path.resolve(__dirname, "src/routes") },
+                { find: "@atoms", replacement: path.resolve(__dirname, "src/atoms") },
+                { find: "@workarounds", replacement: path.resolve(__dirname, "src/workarounds") },
+                {
+                    find: "tailwind-config",
+                    replacement: path.resolve(__dirname, "./tailwind.config.js"),
+                },
+            ],
+        },
+        base: mode === "production" ? GH_PAGE_BASE : "",
+    };
 });
