d33374562c5d2955264a8625a4119e6aafee7902
Newish0 <80089521+Newish0@users.noreply.github.com>
2023-10-09 18:02:58 -0700

    docs(workspace): add docs to all functions

--- src/util/Workspace.ts
+++ src/util/Workspace.ts
@@ -40,10 +40,17 @@ export default class Workspace {
         workspaceInfo: (id: string) => `_workspace/${id}/info`,
     } as const;
 
+    /** Contains all workspaces */
     private static store = new Map<string, Workspace>();
 
+    /** ID of the current workspace */
     private id: string;
 
+    /**
+     * Returns the workspace of the specified ID. Used for instantiating a `Workspace`.
+     * @param id the id of the specified workspace
+     * @returns a workspace instance
+     */
     public static async get(id: string) {
         let ws = Workspace.store.get(id);
 
@@ -54,6 +61,11 @@ export default class Workspace {
         return ws;
     }
 
+    /**
+     * Restore all workspaces stored in DB. This function must be run at the start of the app
+     * (called at the bottom of the file).
+     * @returns
+     */
     public static async restore() {
         const wsIndex = await localforage.getItem<string[]>(Workspace.DB_KEY.workspaceIndex());
 
@@ -65,10 +77,10 @@ export default class Workspace {
     }
 
     /**
-     *
-     * @param hash
-     * @param exclude list of workspace ids
-     * @returns
+     * Check if a file is in use by the workspaces.
+     * @param hash the hash of the file
+     * @param exclude list of id of the workspaces to exclude
+     * @returns true if in use else false
      */
     public static async fileIsInuse(hash: string, exclude?: string[]) {
         for (const [id, ws] of Workspace.store) {
@@ -83,6 +95,10 @@ export default class Workspace {
         this.id = id;
     }
 
+    /**
+     * Get a list of hashes of the files used by this workspace.
+     * @returns
+     */
     private async getFileHashList() {
         const fileIndex = await localforage.getItem<string[]>(
             Workspace.DB_KEY.workspaceFilesIndex(this.id)
@@ -91,6 +107,10 @@ export default class Workspace {
         return fileIndex ?? [];
     }
 
+    /**
+     * Update the file hash list of this workspace.
+     * @param fileHashList
+     */
     private async setFileHashList(fileHashList: string[]) {
         await localforage.setItem<string[]>(
             Workspace.DB_KEY.workspaceFilesIndex(this.id),
@@ -98,17 +118,28 @@ export default class Workspace {
         );
     }
 
+    /**
+     * Get a file from DB.
+     */
     private async getFile(hash: string) {
         if (!(await this.getFileHashList()).includes(hash)) return null;
 
         return await localforage.getItem<File>(Workspace.DB_KEY.file(hash));
     }
 
+    /**
+     * Delete a file from DB.
+     * @param hash hash of the file
+     * @returns the deleted file
+     */
     private async deleteFile(hash: string) {
         const fileHashList = await this.getFileHashList();
 
         if (!fileHashList.includes(hash)) return null;
+
         const file = await localforage.getItem<File>(Workspace.DB_KEY.file(hash));
+
+        // Only actually delete the file if it's not in use by all other workspaces.
         if (await Workspace.fileIsInuse(hash, [this.id])) {
             return file;
         } else {
@@ -124,19 +155,32 @@ export default class Workspace {
         }
     }
 
+    /**
+     * Stores a file to DB.
+     * @param file file to store
+     * @param hash optional: the hash of the file
+     * @returns The newly added file
+     */
     private async addFile(file: File, hash?: string) {
         const fileHashList = await this.getFileHashList();
 
         if (!hash) hash = await hashArrayBuffer(await file.arrayBuffer());
+
+        // Skip storing the file if already in DB.
         if (fileHashList.includes(hash))
             return await localforage.getItem<File>(Workspace.DB_KEY.file(hash));
 
+        // Update file hash list with the hash of the new file
         fileHashList.push(hash);
         await this.setFileHashList(fileHashList);
 
         return await localforage.setItem<File>(Workspace.DB_KEY.file(hash), file);
     }
 
+    /**
+     * Get the list of items from DB
+     * @returns List of ExtendedDNDItem as a promise
+     */
     public async getItems(): Promise<ExtendedDNDItem[]> {
         const fileHashList = await this.getFileHashList();
 
@@ -148,14 +192,15 @@ export default class Workspace {
 
         if (!minItems) return dndItems;
 
+        // Get the proxy pages of the stored files
         const proxyPagesMap = new Map<string, ProxyPage[]>();
-
         for (const hash of fileHashList) {
             const file = await this.getFile(hash);
             if (!file) continue;
             proxyPagesMap.set(hash, await (await BetterPDF.open(file)).toProxyPages(0.75));
         }
 
+        // Expand all MinimalDNDItem to ExtendedDNDItem
         for (const minItem of minItems) {
             const proxyPages = proxyPagesMap.get(minItem.fileHash);
             if (!proxyPages) throw new Error("Failed to restore workspace items");
@@ -169,6 +214,10 @@ export default class Workspace {
         return dndItems;
     }
 
+    /**
+     * Store a list of ExtendedDNDItem to DB
+     * @returns
+     */
     public async setItems(items: ExtendedDNDItem[]) {
         const fileHashList = await this.getFileHashList();
 
@@ -176,6 +225,7 @@ export default class Workspace {
 
         const minItems: MinimalDNDItem[] = [];
 
+        // Convert all ExtendedDNDItem to MinimalDNDItem
         for (let i = 0; i < items.length; i++) {
             const item = items[i];
             itemFileHashObj[item.page.reference.hash] = true;
@@ -204,6 +254,11 @@ export default class Workspace {
         }
     }
 
+    /**
+     * Updates the info of this workspace.
+     * @param info
+     * @returns
+     */
     public async setInfo(info: WorkspaceInfo) {
         return await localforage.setItem<WorkspaceInfo>(
             Workspace.DB_KEY.workspaceInfo(this.id),
@@ -211,9 +266,14 @@ export default class Workspace {
         );
     }
 
+    /**
+     * Returns the info of this workspace.
+     * @returns
+     */
     public async getInfo() {
         return await localforage.getItem<WorkspaceInfo>(Workspace.DB_KEY.workspaceInfo(this.id));
     }
 }
 
+// Load workspaces from DB at the stare of the app.
 Workspace.restore();
